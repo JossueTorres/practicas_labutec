199b6e5d163a916691d6c320ab18f36114db7ff5
Mauricio Rivera <49140795+Rivera-EM@users.noreply.github.com>
2019-05-21 11:36:28 -0600

    Interfaces

---
+++ app/src/main/java/com/example/practicaslibres/fm_Practicas.java
@@ -0,0 +1,18 @@
+package com.example.practicaslibres;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fm_Practicas extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fm__practicas, container, false);
+    }
+}
