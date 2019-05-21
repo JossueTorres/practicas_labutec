10350cb3ff119755819b5125451062d3b53db54f
kevin <kevin.melendezlipes@gmail.com>
2019-05-21 05:26:11 -0300

    menu v2

--- app/src/main/java/com/example/practicaslibres/MenuEncargado.java
+++ app/src/main/java/com/example/practicaslibres/MenuEncargado.java
@@ -56,7 +56,7 @@ NavigationView na1;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        getMenuInflater().inflate(R.menu.menu_encargado, menu);
         return true;
     }
 
