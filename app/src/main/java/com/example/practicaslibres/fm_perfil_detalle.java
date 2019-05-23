0cd38f6209b380c08d3e01ff9d6f37124e104dea
Oscar Torres <40122963+JossueTorres@users.noreply.github.com>
2019-05-23 08:32:09 -0600

    Alumno perfil
    Perfil del alumno creacion base.

---
+++ app/src/main/java/com/example/practicaslibres/fm_perfil_detalle.java
@@ -0,0 +1,20 @@
+package com.example.practicaslibres;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fm_perfil_detalle extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fm_perfil_detalle, container, false);
+    }
+
+}
