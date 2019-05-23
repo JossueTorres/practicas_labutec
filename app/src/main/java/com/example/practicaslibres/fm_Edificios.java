1de706634fd621c93eabf7ba550e0146e75eacf3
ecrespin12 <ecrespin.612@gmail.com>
2019-05-22 20:41:30 -0600

    Consumir Servicios Volley
    Consumir ws con volley

--- app/src/main/java/com/example/practicaslibres/fm_Edificios.java
+++ app/src/main/java/com/example/practicaslibres/fm_Edificios.java
@@ -1,5 +1,6 @@
 package com.example.practicaslibres;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,10 +11,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class fm_Edificios extends Fragment {
@@ -22,6 +39,7 @@ public class fm_Edificios extends Fragment {
 
     //objetos
     public Button btn_agregar;
+    private ListView lvEdificio;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,7 +47,9 @@ public class fm_Edificios extends Fragment {
         View view= inflater.inflate(R.layout.fragment_fm__edificios, container, false);
 
         btn_agregar = view.findViewById(R.id.btn_Agregar);
+        lvEdificio = view.findViewById(R.id.lv_Edificios);
 
+        obtenerEdificios_ws();
         btn_agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,10 +58,64 @@ public class fm_Edificios extends Fragment {
 
                 //dialog a invocar
                 fm_dialogEdificio dialog = new fm_dialogEdificio();
+
                 dialog.show(getFragmentManager(), "dialogListaEdificio");
             }
         });
+
+
         return view;
+    }
+
+
+    //consumir Servicio
+    public void obtenerEdificios_ws() {
+
+        //url
+        String GET_URL="http://104.248.185.225/practicaslab_utec/apis/admin/Edificio_api/listEdificios2";
+        final ProgressDialog loading = ProgressDialog.show(getContext(), "Por favor espere...", "Cargando Informacion",
+                false, false);
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, GET_URL,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //mostrar metodo
+                        loading.dismiss();
+                        mostrarListView(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loading.dismiss();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(jsonObjReq);
+
+    }
+
+    //Mostrar el servicio en listview
+
+    private void mostrarListView(JSONObject obj) {
+
+        try {
+            List<String> edificios = new ArrayList<String>();
+            JSONArray lista = obj.optJSONArray("resp");
+            for(int i =0; i<lista.length();i++){
+                JSONObject json_data = lista.getJSONObject(i);
+                String edf = json_data.getString("edf_acronimo") + " - " + json_data.getString("edf_nombre");
+                edificios.add(edf);
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, edificios);
+            lvEdificio.setAdapter(adapter);
+
+        } catch (Exception ex){
+            Toast.makeText(getContext(), "Error al cargar la lista" + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }finally {
+
+        }
     }
 
 
--- app/src/main/java/com/example/practicaslibres/fm_dialogEdificio.java
+++ app/src/main/java/com/example/practicaslibres/fm_dialogEdificio.java
@@ -1,5 +1,6 @@
 package com.example.practicaslibres;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,24 +12,39 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class fm_dialogEdificio extends DialogFragment {
 
     private static final String TAG ="dialogListaEdificio"; //nombre de fragment
 
     //objetos
-    private EditText edt1, edt2;
+    private EditText edtNombre, edtAcronimo;
     public TextView tvOk, tvCancel;
     public Button btn_guardar,btn_salir,btn_eliminar;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.dialog_edificos, container, false);
-        edt1 = view.findViewById(R.id.edt_lab_nombre);
+
         //objetos
         btn_guardar = view.findViewById(R.id.btnEdificio_guardar);
         btn_salir = view.findViewById(R.id.btnEdificio_salir);
         btn_eliminar = view.findViewById(R.id.btnEdificio_Eliminar);
+
+        edtNombre = view.findViewById(R.id.edt_edf_nombre);
+        edtAcronimo = view.findViewById(R.id.edt_edf_acronimo);
 
         //Para cancelar
         btn_salir.setOnClickListener(new View.OnClickListener() {
@@ -47,11 +63,7 @@ public class fm_dialogEdificio extends DialogFragment {
             public void onClick(View v) {
 
                 Log.d(TAG, "Guardando...");
-                String input = edt1.getText().toString();
-                if(!input.equals("")){
-                    //aca toda la logica
-
-                }
+                registrarEdificio_ws();
             }
         });
         //para eliminar
@@ -60,14 +72,62 @@ public class fm_dialogEdificio extends DialogFragment {
             public void onClick(View v) {
 
                 Log.d(TAG, "Eliminando...");
-                String input = edt1.getText().toString();
-                if(!input.equals("")){
-                    //aca toda la logica
 
-                }
             }
         });
 
         return view;
     }
+
+    //--- Metodo para consumir servicios por volley---
+
+    private void registrarEdificio_ws(){
+
+        //Cargando, barra de progreso...
+        final ProgressDialog loading = ProgressDialog.show(getContext(), "Por favor espere...", "Registrando Edificio",
+                false, false);
+
+        //Url de Servicio
+        String POST_URL = "http://104.248.185.225/practicaslab_utec/apis/admin/Edificio_api/insertEdificio";
+
+        //Solicitud de cadena
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, POST_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loading.dismiss();
+                Toast.makeText(getContext(), response, Toast.LENGTH_LONG).show();
+
+
+            }
+        }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    loading.dismiss();
+                    Toast.makeText(getContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+            })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+
+                //Parametros de ws
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("txtNombre", edtNombre.getText().toString());
+                params.put("txtAcronimo", edtAcronimo.getText().toString());
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+
+    }
+
+
+
+
+
+
+
 }
